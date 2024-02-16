0385ab64d788c3da41453597a47ea41e0a426cf1
ninrh <jinseok081540@gmail.com>
2024-02-16 14:51:02 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById('click-sky').addEventListener('click', function () {
+  window.location.href = '플레이어 등급/SKY.html';
+});
+
+document.getElementById('click-cloud').addEventListener('click', function () {
+  window.location.href = '플레이어 등급/CLOUD.html';
+});
+
+document.getElementById('click-야생').addEventListener('click', function () {
+  window.location.href = '서버/야생.html';
+});
+
+document.getElementById('click-build').addEventListener('click', function () {
+  window.location.href = '서버/건축.html';
+});
+
+document.getElementById('click-spawn').addEventListener('click', function () {
+  window.location.href = '서버/스폰.html';
+});
+
+document.getElementById('click-menu').addEventListener('click', function () {
+  window.location.href = '메뉴/대충.html';
+});
+
+document.getElementById('click-menu_1').addEventListener('click', function () {
+  window.location.href = '메뉴/설명.html';
+});
+
+document.getElementById('click-setting').addEventListener('click', function () {
+  window.location.href = '기본/설정.html';
+});
